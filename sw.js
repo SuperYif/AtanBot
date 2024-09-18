14318e00717bd8977077cabd79f8cba45bad6461
SuperYif <tianyf98@gmail.com>
2024-09-18 01:43:28 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,56 @@
+const CACHE_NAME = 'ifbot-cache-v1';
+const urlsToCache = [
+  '/ifBot./',
+  '/ifBot./index.html',
+  '/ifBot./manifest.json',
+  '/ifBot./icon-192x192.png',
+  '/ifBot./icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/showdown/2.1.0/showdown.min.js'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/ifBot./',
-        '/ifBot./index.html',
-        '/ifBot./manifest.json',
-        '/ifBot./icon-192x192.png',
-        '/ifBot./icon-512x512.png'
-        // 添加其他需要缓存的资源
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .catch((error) => console.error('Cache addAll failed:', error))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          (response) => {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
